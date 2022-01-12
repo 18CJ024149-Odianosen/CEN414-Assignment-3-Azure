97207f0317cc7bb9f7003a7b18e7102a684b7db6
18CJ024149-Odianosen <92312227+18CJ024149-Odianosen@users.noreply.github.com>
2022-01-12 14:55:22 -0800

    These are visualisations showing the Number of Homicides per 100,000 in Nigeria from 2010-2019.

--- app.py
+++ app.py
@@ -14,25 +14,22 @@ def index():
 @app.route('/refresh_plots', methods=['GET'])
 def refresh_plots():
 
-    #connect to db
-    CONNECTION_STRING = "mongodb+srv://owolabioromidayo:thisisdayo99@cluster0.28vgr.mongodb.net/gdp_country?retryWrites=true&w=majority"
-    client = MongoClient(CONNECTION_STRING)
-    collection = client['gdp_country']['gdp_country']
-    cursor = collection.find()
+
     
     #get and mutate dataframe 
-    df = pd.DataFrame(list(cursor))
-    df = df.set_index('Reference Area')
-    del df['_id']
-    df['Time Period'] = pd.to_numeric(df['Time Period'])
-    df['Observation Value'] = pd.to_numeric(df['Observation Value'])
-    df = df.sort_values('Observation Value')
-    print(df)
-    
+    for i in range(1,10)
+        df = pd.read_csv(f'Homicide{i}_Database.csv')
+
+        df.pivot_table(index='Country', columns = 'Sex', values = f'{2009+i}').plot(kind = 'bar', figsize=(15,5))
+
+        plt.xlabel('Country')
+        plt.ylabel(f'{2009+i}')
+        plt.title('Homicides Male and Female Count per 100,000')
+        plt.legend(loc='upper right')
+        plt.savefig(f'./static/images/Azure{i}.png')
+        # plt.show()
+        
     #generate plots
-    bar_plots(df)
-    dispersion_plots(df)
-    box_plots(df)
 
     return redirect("/")
 
